063e5346d8c33bd0ca6ebf263d60499d0a650329
George Ivanov <george.ivanov@ecentria.com>
2022-01-31 16:25:13 -0600

    Refactor script

--- telegram/index.ts
+++ telegram/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import fs from 'fs';
+import bigInt from 'big-integer';
 import { TelegramClient, Api } from 'telegram';
 import { StringSession } from 'telegram/sessions';
 import { initializeApp } from 'firebase/app';
@@ -15,16 +16,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log({
-  apiKey: process.env.DB_API_KEY as string,
-  authDomain: process.env.DB_AUTH_DOMAIN as string,
-  projectId: process.env.DB_PROJECT_ID as string,
-  storageBucket: process.env.DB_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.DB_MESSAGING_SENDER_ID as string,
-  appId: process.env.DB_APP_ID as string,
-  measurementId: process.env.DB_MEASUREMENT_ID as string
-});
-
 const app = initializeApp({
   apiKey: process.env.DB_API_KEY as string,
   authDomain: process.env.DB_AUTH_DOMAIN as string,
@@ -37,7 +28,7 @@ const app = initializeApp({
 
 const db = getDatabase(app);
 
-(async () => {
+const connect = async () => {
   const client = new TelegramClient(stringSession, parseInt(process.env.API_ID as string), process.env.API_HASH as string, {
     connectionRetries: 10,
   });
@@ -48,47 +39,78 @@ const db = getDatabase(app);
       await rl.question('Please enter the code you received: '),
     onError: (err) => console.log(err),
   });
+  
+  return client;
+};
 
-  const queue: any[] = [];
-  const chatIds = (await get(ref(db, 'telegram/chatIds'))).val();
+(async () => {
+  const client = await connect();
+  const queue: bigInt.BigInteger[] = [];
+  let sentQty = 0;
 
-  for(const chatId of chatIds) {
-    try {
-      const info = await client.invoke(new Api.channels.GetFullChannel({ channel: chatId, }));
-      const result = await client.getParticipants(chatId, { limit: (info.fullChat as any).participantsCount });
-      const logs = (await get(ref(db, `telegram/logs`))).val() || {};
+  const fetch = async () => {
+    const chatIds = (await get(ref(db, 'telegram/chatIds'))).val();
 
-      queue.push(...(result.filter(({ id, username, firstName, lastName }) => {
-        if (exclusions.fullName.includes(String(`${firstName} ${lastName}`).trim()) || exclusions.username.includes(username as string)) {
-          return false;
+    for(const chatId of chatIds) {
+      try {
+        const info = await client.invoke(new Api.channels.GetFullChannel({ channel: chatId, })) as unknown as { participantsCount: number };
+        const result = await client.getParticipants(chatId, { limit: info.participantsCount });
+        
+        console.log('Fetched', result.length, 'members of', chatId);
+
+        const logs = (await get(ref(db, `telegram/logs`))).val() || {};
+
+        queue.push(...(result.filter(({ id, username, bot, firstName, lastName }) => {
+          if (bot || exclusions.fullName.includes(String(`${firstName} ${lastName}`).trim()) || exclusions.username.includes(username as string)) {
+            return false;
+          }
+          return !logs[id.toString()];
+        }).map(({ id, username, firstName, lastName }) => {
+          console.log('Discovered', id.toString(), username, firstName, lastName);
+          return id;
+        })));
+      } catch(e) {
+        if (e instanceof Error) {
+          console.error('Unable to fetch participants', chatId, e.message);
         }
-        return !logs[id.toString()];
-      }).map(({ id, username, firstName, lastName }) => {
-        console.log('Discovered', id.toString(), username, firstName, lastName);
-        return id;
-      })));
-    } catch(e) {
-      console.error('Unable to fetch participants', chatId, e);
-    }
-  }
-
-  const message = fs.readFileSync('./message.txt').toString('utf8');
-
-  const sendMesage = async () => {
-    if (queue.length > 0) {
-      const id = queue.pop();
-      if (await client.sendMessage(id, { message })) {
-        set(ref(db, `telegram/logs/${id}`), true);
-        console.log('Message sent to', id);
       }
-      setTimeout(sendMesage, parseInt(process.env.SEND_TIMEOUT as string));
-    } else {
-      console.log('Queue is empty. Skipping');
     }
   };
 
-  await sendMesage();
+  const start = async () => {
+    const SEND_TIMEOUT = parseInt(process.env.SEND_TIMEOUT as string);
+    const SEND_THRESHOLD = parseFloat(process.env.SEND_THRESHOLD as string);
+    
+    const message = fs.readFileSync('./message.txt').toString('utf8');
+    const dequeue = async () => {
+      if (queue.length > 0) {
+        const id = queue.pop();
+        try {
+          if (id && await client.sendMessage(id, { message })) {
+            await set(ref(db, `telegram/logs/${id}`), true);
+            sentQty++;
+            console.log('Message sent to', id);
+          }
+        } catch(e) {
+          if (e instanceof Error) {
+            console.log('Unable to send a message', e.message);
+          }
+        }
+        const timeout = SEND_TIMEOUT + (SEND_TIMEOUT * SEND_THRESHOLD * sentQty);
+        console.log('Next will be sent in', timeout);
+        setTimeout(dequeue, timeout);
+      } else {
+        console.log('Queue is empty. Skipping');
+      }
+    };
 
-  console.log('You should now be connected.');
-  console.log(client.session.save());
+    await dequeue();
+  };
+
+  await fetch();
+  await start();
+
+  process.on('beforeExit', () => {
+    client.session.save();
+  });
 })();
